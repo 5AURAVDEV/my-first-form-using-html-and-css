78ad7bd4f3db29dccf4dfb32446e2d659bb53461
Saurav Dev <devofficial998@gmail.com>
2023-08-23 08:37:31 +0530

    edit functionality added

--- app.js
+++ app.js
@@ -1,41 +1,3 @@
-/*function print(event){
-    let first_name=fname.value
-    let middle_name=document.getElementById('mname').value
-    let last_name=document.getElementById('lname').value
-    let e_mail=document.getElementById('addr').value
-    let birthday=document.getElementById('b-day').value
-    let password=document.getElementById('pass').value
-    let country=document.getElementById('std').value
-    let mobile_no=document.getElementById('ph-no').value
-    /*document.write(`your name is ${first_name} ${middle_name} ${last_name}<br>`)
-    document.write(`your phone number is ${mobile_no}<br>`)
-    document.write(`your birthday is ${birthday}<br>`)
-    document.write(`your e-mail is ${e_mail}<br>`)
-    document.write(`your password is ${password}<br>`)
-
-
-event.preventDefault()
-//console.log(event)
-console.log(first_name)
-console.log(middle_name)
-console.log(last_name)
-console.log(e_mail)
-console.log(birthday)
-console.log(password)
-console.log(mobile_no)
-}*/
-
-
-
-/*function print_details(event){
-    event.preventDefault()
-    localStorage.setItem('first_name',document.getElementById('fname').value)
-    localStorage.setItem('middle_name',document.getElementById('mname').value)
-    localStorage.setItem('last_name',document.getElementById('lname').value)
-    localStorage.setItem('bithday_name',document.getElementById('b-day').value)
-    localStorage.setItem('e-mail',document.getElementById('addr').value)
-}*/
-
 let myform=document.getElementById("myform")
 myform.addEventListener("submit",print_details)
 
@@ -60,6 +22,14 @@ function print_details(event){
     li.appendChild(btn)
     x.appendChild(li)
     btn.addEventListener("click",remove_details)
+    let editbtn=document.createElement("button")
+    editbtn.setAttribute("type","button")
+    editbtn.innerText="edit"
+    editbtn.className="EDIT"
+    editbtn.style.backgroundColor="yellow"
+    li.appendChild(editbtn)
+    x.appendChild(li)
+    editbtn.addEventListener("click",change_details)
 }
 /*let a=document.getElementById("added")
 a.addEventListener("click",function (event){
@@ -69,8 +39,21 @@ a.addEventListener("click",function (event){
 })*/
 function remove_details(event) {
     let b=event.target.parentElement
-    let key=b.innerText.replace("del","")
+    let key=b.innerText.slice(0,b.innerText.length-7)
+    console.log(b)
     let detail=JSON.parse(key)
+    let k=detail.email
+    localStorage.removeItem(k)
+    b.style.display="none"
+}
+
+function change_details(event){
+    let b=event.target.parentElement
+    let key=b.innerText.slice(0,b.innerText.length-7)
+    let detail=JSON.parse(key)
+    document.getElementById("fname").value=detail.first_name
+    document.getElementById("lname").value=detail.last_name
+    document.getElementById("addr").value=detail.email
     let k=detail.email
     localStorage.removeItem(k)
     b.style.display="none"
